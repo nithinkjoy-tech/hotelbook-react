3eff7afcaa5e7032189c09a03b6678b2de1f6fcf
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-07-13 00:13:34 +0530

    Select option functionality of menu Items is added

--- src/pages/GuestDashboard.jsx
+++ src/pages/GuestDashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import PersonIcon from '@material-ui/icons/Person';
 import HistoryIcon from '@material-ui/icons/History';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
@@ -15,19 +15,23 @@ function GuestDashboard() {
         phone:'1234567890',
         email : 'vishnusatheeshpulickal555@gmail.com'
     }
+    const [selectOption,setSelectOption] = useState('history')
     return (
         <div className="guestDashboard">
             <div className="guestDashboard_menu">
                 <ul className="guestDashboard_menuItems">
-                    <li><PersonIcon /><div className="Content">Profile</div></li>
-                    <li><HistoryIcon /><div className="Content">History</div></li>
-                    <li><EventAvailableIcon/><div className="Content">Booked</div></li>
+                    <li onClick={e => setSelectOption('profile')}><PersonIcon /><div className="Content">Profile</div></li>
+                    <li onClick={e => setSelectOption('history')}><HistoryIcon /><div className="Content">History</div></li>
+                    <li onClick={e => setSelectOption('booked')}><EventAvailableIcon/><div className="Content">Booked</div></li>
                     <li><ExitToAppIcon/><div className="Content">Logout</div></li>
                 </ul>
             </div>
-            {/* <Profile title={"Profile"} description={"Basic info, for a faster booking experience"} details={data}/> */}
-               <Booked_Dashboard />
-            {/* <History /> */}
+           
+            {(() => { if (selectOption === 'profile') return (<Profile title={"Profile"} description={"Basic info, for a faster booking experience"} details={data}/>)
+             else if (selectOption === 'history') return (<History />)
+             else if (selectOption === 'booked') return (<Booked_Dashboard />)
+             })()}
+
             {/* <div className="guestDashboard_details">
                <DashboardContent />
             </div> */}
