f257db674dba654f1a96b1e86e9bd04679b74191
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-07-14 21:46:28 +0530

    Menu section is created for renter dashboard

--- src/pages/RenterDashboard.jsx
+++ src/pages/RenterDashboard.jsx
@@ -2,10 +2,17 @@ import React, {useEffect,useState} from "react";
 import ReactLoading from 'react-loading'
 import SearchResultComponent from "./../components/searchPageComponent/SearchResultComponent";
 import { getRenterHotels } from './../api/renter';
+import '../../src/css/RenterDashboard.css'
+
+import PersonIcon from '@material-ui/icons/Person';
+import HotelIcon from '@material-ui/icons/Hotel';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 
 function RenterDashboard() {
   const [hotels,setHotels]=useState()
-  const [isLoading,setIsLoading]=useState(true)
+  const [isLoading,setIsLoading]=useState(true);
+  const [selectOption,setSelectOption] = useState('profile');
 
   async function getHotels() {
     const {data}=await getRenterHotels()
@@ -31,8 +38,16 @@ function RenterDashboard() {
   }
 
   return (
-    <div>
-      <SearchResultComponent user="renter" hotels={hotels} />
+    <div className="renterDashboard">
+      <div className="guestDashboard_menu">
+                <ul className="guestDashboard_menuItems">
+                    <li onClick={e => {setSelectOption('profile'); }}><PersonIcon /><div className="Content">Profile</div></li>
+                    <li onClick={e => {setSelectOption('hotels'); }}><HotelIcon /><div className="Content">Hotels</div></li>
+                    <li onClick={e => setSelectOption('booked')}><EventAvailableIcon/><div className="Content">Booked</div></li>
+                    <li><ExitToAppIcon/><div className="Content">Logout</div></li>
+                </ul>
+            </div>
+      {/* <SearchResultComponent user="renter" hotels={hotels} /> */}
     </div> 
   );
 }
