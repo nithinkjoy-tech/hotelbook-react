060af622de6af9ab1043efb7434ba1d5fccc407a
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-07-27 20:35:49 +0530

    Table simple layout is added

---
+++ src/components/common/Table.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Table() {
+    return (
+        <table class="table table-bordered">
+        <thead class="table-dark">
+          <tr>
+              <th scope="col">Room Type</th>
+              <th scope="col">Options</th>
+              <th scope="col">Included in this Price</th>
+              <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+              <td></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+}
+
+export default Table
