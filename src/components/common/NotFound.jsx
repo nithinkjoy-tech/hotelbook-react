79ca78ed838cefe69aed1e1585e1d9fe1f883349
Nithin K Joy <nithinjoy121212@gmail.com>
2021-09-16 15:44:54 +0530

    modified not found page.

--- src/components/common/NotFound.jsx
+++ src/components/common/NotFound.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import "../../css/NotFound.css"
+import ErrorPage from 'react-custom-errors';
 
 function NotFound() {
   return (
-    <div>
-      <aside>
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4424790/Mirror.png"
-          alt="404 Image"
-        />
-      </aside>
-      <main>
-        <h1>Sorry!</h1>
-        <p>
-          Either you aren't cool enough to visit this page or it doesn't exist{" "}
-          <em>. . . like your social life.</em>
-        </p>
-        <button>You can go now!</button>
-      </main>
-    </div>
+    <ErrorPage code="404" link="/dashboard" />
   );
 }
 
