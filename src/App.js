442eb9fc6d995263717ce360fa17416d694dbc31
Nithin K Joy <nithinjoy121212@gmail.com>
2021-09-25 23:05:15 +0530

    Refactored services.js, app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import React from "react";
-import {Switch, Route, Redirect} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-// import SearchPage from "./pages/SearchPage";
 import NavBar from "./components/common/NavBar.jsx";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
 import Footer from "./components/common/Footer";
 import ListPropertyPage from "./pages/ListPropertyPage";
-import {ToastContainer} from "react-toastify";
-// import ListPropertyWelcomePage from "./pages/ListPropertyWelcomePage";
 import GuestDashboard from "./pages/GuestDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import HotelDetails from "./pages/HotelDetails";
@@ -19,13 +15,8 @@ import ReceptionRoute from "./components/common/ReceptionRoute";
 import RestaurantRoute from "./components/common/RestaurantRoute";
 import AdminRoute from "./components/common/AdminRoute";
 import GuestRoute from "./components/common/GuestRoute";
-// import RoomDescription from "./components/RoomDetailsPageComponents/RoomDescription";
 import RoomDetails from "./pages/RoomDetails";
 import BookedRoomDetails from "./components/RoomDetailsPageComponents/BookedRoomDetails";
-// import AdminBook from "./components/common/AdminBook";
-// import Dashboard from "./components/receptionistArea/Dashboard";
-// import OverView from "./components/receptionistArea/OverView";
-import ArrivalList from "./components/receptionistArea/ArrivalList";
 import Sidebar from "./components/receptionistArea/Sidebar";
 import ReceptionDashboard from "./pages/ReceptionDashboard";
 import RestaurantDashboard from "./pages/RestaurantDashboard";
@@ -42,36 +33,31 @@ import ResetPassword from './components/common/ResetPassword';
 import LinkReview from './components/common/LinkReview';
 import NotFound from './components/common/NotFound';
 import Logout from './components/common/Logout';
+import {ToastContainer} from "react-toastify";
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
       <ToastContainer position="top-center" autoClose={5000000} />
       <NavBar />
-
       <Switch>
         <Route exact path="/about" component={AboutPage} />
-        {/* <Route exact path="/search" component={SearchPage} /> */}
         <Route exact path="/signin" component={SigninPage} />
-        <GuestRoute exact path="/dashboard" component={GuestDashboard} />
         <Route exact path="/signup" component={SignupPage} />
         <Route exact path="/logout" component={Logout} />
-        <GuestRoute exact path="/bookedroomdetails" component={BookedRoomDetails} />
         <Route exact path="/forgotpassword" component={ForgotPassword} />
         <Route exact path="/admin/forgotpassword" component={ForgotPassword} />
         <Route exact path="/resetpassword/:token" component={ResetPassword} />
         <Route exact path="/hoteldetails/:hotelId" component={HotelDetails} />
         <Route exact path="/linkreview/:id" component={LinkReview} />
         <Route exact path="/hotel/roomdetails/:roomId" component={RoomDetails} />
-        <ReceptionRoute exact path="/reception/dashboard/checkin/:bookingId" component={BookedCheckIn} />
-        <ReceptionRoute exact path="/reception/dashboard/checkout/:bookingId" component={CheckOut} />
-        <ReceptionRoute exact path="/reception/dashboard" component={Sidebar} />
         <Route exact path="/reception/signin" component={SigninPage} />
-
-        <RestaurantRoute exact path="/restaurant/dashboard" component={RestaurantSidebar} />
         <Route exact path="/restaurant/signin" component={SigninPage} />
-        <RestaurantRoute exact path="/restaurant/addfooditem" component={AddFoodItem} />
-        <RestaurantRoute exact path="/restaurant/additemstobill/:bookingId" component={AddItemsToBill} />
+        <Route exact path="/admin/signin" component={SigninPage} />
+        <Route exact path="/admin/signup" component={SignupPage} />
+        <Route exact path="/" component={LandingPage} />
+
         <AdminRoute exact path="/admin/reception/signup/:hotelId" component={SignupPage} />
         <AdminRoute exact path="/admin/reception/account/:receptionId" component={ReceptionDashboard} />
         <AdminRoute exact path="/admin/restaurant/signup/:hotelId" component={SignupPage} />
@@ -80,8 +66,6 @@ function App() {
           path="/admin/restaurant/account/:restaurantId"
           component={RestaurantDashboard}
         />
-        <Route exact path="/admin/signin" component={SigninPage} />
-        <Route exact path="/admin/signup" component={SignupPage} />
         <AdminRoute exact path="/admin/dashboard" component={AdminDashboard} />
         <AdminRoute exact path="/admin/room/:hotelId" component={RoomCard} />
         <AdminRoute exact path="/admin/addHotel/:id" component={ListPropertyPage} />
@@ -90,7 +74,18 @@ function App() {
         <AdminRoute exact path="/admin/manageHotel/:hotelId" component={HotelSidebar} />
         <AdminRoute exact path="/admin/addroom/:hotelId" component={AddRoom} />
         <AdminRoute exact path="/admin/editroom/:roomId" component={AddRoom} />
-        <Route exact path="/" component={LandingPage} />
+
+        <GuestRoute exact path="/dashboard" component={GuestDashboard} />
+        <GuestRoute exact path="/bookedroomdetails" component={BookedRoomDetails} />
+
+        <ReceptionRoute exact path="/reception/dashboard/checkin/:bookingId" component={BookedCheckIn} />
+        <ReceptionRoute exact path="/reception/dashboard/checkout/:bookingId" component={CheckOut} />
+        <ReceptionRoute exact path="/reception/dashboard" component={Sidebar} />
+
+        <RestaurantRoute exact path="/restaurant/dashboard" component={RestaurantSidebar} />
+        <RestaurantRoute exact path="/restaurant/addfooditem" component={AddFoodItem} />
+        <RestaurantRoute exact path="/restaurant/additemstobill/:bookingId" component={AddItemsToBill} />
+
         <Route component={NotFound} />
       </Switch>
       {window.location.pathname.includes("/reception/dashboard") ||
